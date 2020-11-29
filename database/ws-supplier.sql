a6291d10e2f223c65d02b2f06dd265165554c7ed
Anna Elvira Hartoyo <naelvirahart@gmail.com>
2020-11-29 14:00:17 +0700

    update database

--- database/ws-supplier.sql
+++ database/ws-supplier.sql
@@ -1,75 +1,52 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: localhost
--- Generation Time: Nov 20, 2020 at 11:33 AM
--- Server version: 8.0.19
--- PHP Version: 7.4.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: ws-supplier
+-- ------------------------------------------------------
+-- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `ws-supplier`
---
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `bahan`
 --
 
+DROP TABLE IF EXISTS `bahan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bahan` (
-  `id_bahan` int NOT NULL,
+  `id_bahan` int NOT NULL AUTO_INCREMENT,
   `nama_bahan` varchar(200) DEFAULT NULL,
-  `harga_satuan` decimal(12,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `harga_satuan` decimal(12,2) DEFAULT NULL,
+  PRIMARY KEY (`id_bahan`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `bahan`
 --
 
-INSERT INTO `bahan` (`id_bahan`, `nama_bahan`, `harga_satuan`) VALUES
-(1, 'Gula', '30000.00'),
-(2, 'Cocoa Powder', '15000.00'),
-(3, 'Kacang Mede', '50000.00'),
-(4, 'Wall Nut', '40000.00'),
-(5, 'Susu', '40000.00'),
-(6, 'Vanili', '30000.00'),
-(7, 'Bubuk Green Tea', '65000.00'),
-(8, 'Esens Stroberi', '35000.00'),
-(9, 'Biji Coklat', '100000.00'),
-(10, 'Butter', '55000.00');
+LOCK TABLES `bahan` WRITE;
+/*!40000 ALTER TABLE `bahan` DISABLE KEYS */;
+INSERT INTO `bahan` VALUES (1,'Gula',3000.00),(2,'Cocoa Powder',1500.00),(3,'Kacang Mede',5000.00),(4,'Wall Nut',4000.00),(5,'Susu',4000.00),(6,'Vanili',3000.00),(7,'Bubuk Green Tea',6500.00),(8,'Esens Stroberi',3500.00),(9,'Biji Coklat',10000.00),(10,'Butter',5500.00);
+/*!40000 ALTER TABLE `bahan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bahan`
---
-ALTER TABLE `bahan`
-  ADD PRIMARY KEY (`id_bahan`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `bahan`
---
-ALTER TABLE `bahan`
-  MODIFY `id_bahan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-29 14:00:00
